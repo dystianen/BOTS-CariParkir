ceb317450cbc1e2e65d41d8c46fd37048b0dab20
Hasta Ragil Saputra <hastaragil17@gmail.com>
2021-07-18 16:57:29 +0700

    feat: basic antd

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
+const withAntdLess = require('next-plugin-antd-less');
+const antdVariables = require('./styles/antd_variables');
+
+module.exports = withAntdLess({
+
+  modifyVars: antdVariables,
+
+  lessVarsFilePath: './styles/variables.less',
+
+  lessVarsFilePathAppendToEndOfContent: false,
+
+  cssLoaderOptions: {},
+
+  webpack(config) {
+    return config;
+  },
+
   reactStrictMode: true,
-}
+});
---
+++ styles/antd_variables.js
@@ -0,0 +1,8 @@
+module.exports = {
+    '@primary-color': '#ed1f24',
+    '@body-background': '#e3e8ee',
+    '@font-family': `'Montserrat Alternates', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+  'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+  'Noto Color Emoji'`,
+    '@font-size-base': '12px',
+};
